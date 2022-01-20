b9e8fdbba69c53de4d801928a03e8ab5c630763c
Jessika <jesscodeit@gmail.com>
2022-01-20 14:00:00 -0600

    added notes to program

--- stock_picker.rb
+++ stock_picker.rb
@@ -5,17 +5,28 @@
 =end
 
 def stock_picker(array_of_prices)
+    # create variable to sell the best combo of prices
     best_buy_sell = [0,0]
 
+    # look at each price individually
     array_of_prices.each_with_index do |price, index|
+
+        # set the current price to the buy price
         buy = price
+
+        # while the current price that has atleast one more price following it, do this
         while index <= array_of_prices.length - 1
+
+            # set the current sell price to the current index count
             sell = array_of_prices[index]
+
+            # if the current profit is bigger than the saved best profit, replace it
             if sell - buy > best_buy_sell[1] - best_buy_sell[0]
                 best_buy_sell[0] = buy
                 best_buy_sell[1] = sell
                 index += 1
             else
+                # increase index count either way
                 index += 1
             end
         end
